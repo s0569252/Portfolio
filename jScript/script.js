1771b9fe52e585d9fee79e917abb4cd49e8842a1
ImdiMelvana <imdimelvana@gmail.com>
2025-12-21 23:22:09 +0100

    v2

--- jScript/script.js
+++ jScript/script.js
@@ -1,1 +1,6 @@
-console.log('Portfolio loaded');
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
